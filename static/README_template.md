5c548184cdd8bda3c8506663c4f3602175c61657
sota1235 <sota1235@sota1235-no-MacBook-Air.local>
2014-10-06 17:39:36 +0900

    add static folder | alias用のファイルを追加

---
+++ static/README_template.md
@@ -0,0 +1,26 @@
+Name
+====
+
+Overview
+
+### Description
+
+### Demo
+
+### VS.
+
+### Requirement
+
+### Usage
+
+### Install
+
+### Contribution
+
+### Licence
+
+This software is released under the MIT License, see LICENSE.txt.
+
+## Author
+
+[@sota1235](https://github.com/sota1235)
--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,17 +1,17 @@
 #!/usr/bin/zsh
 
 # Remote Login
-alias sota     = "ssh sota@sota1235.com"
-alias t11460ss = "ssh t11460ss@ccx00.sfc.keio.ac.jp"
-alias webedit  = "ssh t11460ss@webedit.sfc.keio.ac.jp"
+alias sota="ssh sota@sota1235.com"
+alias t11460ss="ssh t11460ss@ccx00.sfc.keio.ac.jp"
+alias webedit="ssh t11460ss@webedit.sfc.keio.ac.jp"
 
 # Editor
-alias vi = "vim"
+alias vi="vim"
 
 # Development
-alias readmeinit  = "cp ~/data/README_template.md ~/README.md"
-alias licenseinit = "cp ~/data/LICENSE.txt ~/LICENSE.txt"
+alias readmeinit="cp ~/data/README_template.md ~/README.md"
+alias licenseinit="cp ~/data/LICENSE.txt ~/LICENSE.txt"
 
 # Change Directory
-sota1235 = "cd ~/Documents/sota1235"
-github   = "cd ~/Documents/sota1235/github"
+sota1235="cd ~/Documents/sota1235"
+github="cd ~/Documents/sota1235/github"
