f9a69d806fb3cc87ca7c36a4a6ec8b74d42b33ea
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-11-18 14:50:57 +0900

    middleman周り追加

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -11,20 +11,24 @@ alias -s txt=vim
 
 ## Normal Alias ##
 # Remote Login
-alias sota="ssh sota@sota1235.com"
+alias sota="ssh sota@sota1235.net"
 alias t11460ss="ssh t11460ss@ccx00.sfc.keio.ac.jp"
 alias webedit="ssh t11460ss@webedit.sfc.keio.ac.jp"
 
 # Editor
 alias vi="vim"
 
-# Development
+# Development for git init
 alias readmeinit="cp ~/dotfiles/static/README_template.md ./README.md"
 alias licenseinit="cp ~/dotfiles/static/LICENSE.txt ./LICENSE.txt"
 
 # Change Directory
 alias sota1235="cd ~/Documents/sota1235"
 alias github="cd ~/Documents/sota1235/github"
+alias blog="cd ~/Documents/sota1235/Now_Running/blog"
+
+# Middleman
+alias newpost="middleman article"
 
 # Others
 alias grep="grep --color"
