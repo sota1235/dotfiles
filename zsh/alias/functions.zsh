ee5d51486bc5b06eb61d98c9696835feaa51c64c
sota1235 <t11460ss@sfc.keio.ac.jp>
2014-12-15 17:25:45 +0900

    rmをゴミ箱移動に変更し、補完関数を実装

--- zsh/alias/functions.zsh
+++ zsh/alias/functions.zsh
@@ -4,35 +4,11 @@
 function rmf() {
   for file in $*
   do
-    __rm_single_file $file
+    mv $file ~/.Trash
   done
 }
 
-function __rm_single_file(){
-if ! [ -d ~/.Trash/ ]
-  then
-    command /bin/mkdir ~/.Trash
-  fi
-
-  if ! [ $# -eq 1 ]
-  then
-    echo "__rm_single_file: 1 argument required but $# passed."
-    exit
-  fi
-
-  if [ -e $1 ]
-  then
-    BASENAME=`basename $1`
-    NAME=$BASENAME
-    COUNT=0
-    while [ -e ~/.Trash/$NAME ]
-    do
-      COUNT=$(($COUNT+1))
-      NAME="$BASENAME.$COUNT"
-    done
-
-    command /bin/mv $1 ~/.Trash/$NAME
-  else
-    echo "No such file or directory: $file"
-  fi
+function _rmf(){
+  _arguments \
+    '*: :_files'
 }
