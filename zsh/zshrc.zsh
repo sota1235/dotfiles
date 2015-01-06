e0c9c9b616bc9de9567dc2c51e63138372f31deb
sota1235 <t11460ss@sfc.keio.ac.jp>
2015-01-06 15:34:07 +0900

    https://gist.github.com/mollifier/4979906を参考に付け足した

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,47 @@
+
+# 色を使用できるようにする
+autoload -Uz colors
+colors
+
+###
+# 補完
+###
+
+# 補完機能を有効にする
+autoload -Uz compinit
+compinit
+
+# 補完で小文字/大文字にマッチさせる
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# ../ の後は今いるディレクトリを補完しない
+zstyle ':completion:*' ignore-parents parent pwd ..
+
+# sudo の後ろでコマンド名を補完する
+zstyle ':completion:*:sudo:*' command-path /user/local/sbin /usr/local/bin \
+  /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
+
+# ps コマンドのプロセス名補完
+zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
+
+###
+# オプション
+###
+
+# 日本語ファイル名を表示可能にする
+setopt print_eight_bit
+
+# ディレクトリ名だけでcdする
+setopt auto_cd
+
+# cd したら自動的にpushdする
+setopt auto_pushd
+# 重複したディレクトリを追加しない
+setopt pushd_ignore_dups
+
+# 高機能なワイルドカード展開を使用する
+setopt extended_glob
+
 ###
 #
 # History
