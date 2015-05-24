de7298cd4eec426bda00675a4d530d02ba0656c5
sota1235 <sota1235@gmail.com>
2015-05-24 15:44:09 +0900

    modify: aliasadd関数追加

--- zsh/alias/functions.zsh
+++ zsh/alias/functions.zsh
@@ -23,3 +23,12 @@ function _rmf(){
 function sjis_grep() {
   grep `echo $1 | nkf -s` $2 | nkf -w
 }
+
+###
+# add new alias
+###
+
+# TODO: 引数の個数等、エラー処理
+function aliasadd() {
+  echo "alias $1=\"$2\"" >> ~/.dotfiles/zsh/alias/addalias.zsh
+}
