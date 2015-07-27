2b03d6d41cad487ac3e14b88fefcfe04940623cd
sota1235 <sota1235@gmail.com>
2015-07-27 10:00:00 +0900

    modify: manページをハイライト

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -242,6 +242,24 @@ zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 ###
+# 各種ハイライト
+###
+# manページに色付け
+# TODO: OS X環境で確認
+export MANPAGER='less -R'
+man() {
+    env \
+        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+        LESS_TERMCAP_md=$(printf "\e[1;31m") \
+        LESS_TERMCAP_me=$(printf "\e[0m") \
+        LESS_TERMCAP_se=$(printf "\e[0m") \
+        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+        LESS_TERMCAP_ue=$(printf "\e[0m") \
+        LESS_TERMCAP_us=$(printf "\e[1;32m") \
+        man "$@"
+}
+
+###
 # オプション
 ###
 
