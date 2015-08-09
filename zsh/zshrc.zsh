49eb159fbe41c62d34773626f0873d2def31a812
sota1235 <sota1235@gmail.com>
2015-08-09 09:34:07 +0900

    fix: nvmの読み込みを公式に合わせた

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -323,7 +323,8 @@ fi
 
 ### nvm ###
 if [ -e $HOME/.nvm ]; then
-  source $HOME/.nvm/nvm.sh
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 fi
 
 ### peco ###
