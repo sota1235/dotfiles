f5863a57da2afe6011a848bfbf28eb4b35f3e3a6
sota1235 <sota1235@gmail.com>
2015-08-09 09:45:41 +0900

    modify: pyenv用設定を公式を参考にzshenvに追記

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,5 +1,12 @@
 # export envs
 export LANG=ja_JP.UTF-8
 
+# for pyenv
+if [ -e $HOME/.pyenv ] ; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
+
 # vimが好き
 export EDITOR=vim
