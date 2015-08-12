696340fc863bc63ca1b6b0a3ca9cec952969c7b8
sota1235 <sota1235@gmail.com>
2015-08-12 22:12:44 +0900

    fix: fix indent

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -4,7 +4,7 @@
 
 # tmuxを起動する
 if which tmux > /dev/null 2>&1 ; then
-[[ -z "$TMUX" && ! -z "$PS1" ]] && tmux
+  [[ -z "$TMUX" && ! -z "$PS1" ]] && tmux
 fi
 
 # キーバインドをEmacs風に
@@ -43,87 +43,87 @@ zstyle ':vcs_info:git:*' formats "(%s):%F{blue}[%f%F{red}%b%f%F{blue}]%f %c%u"
 zstyle ':vcs_info:git:*' actionformats "(%s):[%b]" "%m" "<!%a>"
 
 if is-at-least 4.3.10; then
-    # git 用のフォーマット
-    # git のときはステージしているかどうかを表示
-    zstyle ':vcs_info:git:*' formats '(%s):%F{blue}[%f%F{red}%b%f%F{blue}]%f' '%c%u %m'
-    zstyle ':vcs_info:git:*' actionformats '(%s):[%b]' '%c%u %m' '<!%a>'
-    zstyle ':vcs_info:git:*' check-for-changes true
-    zstyle ':vcs_info:git:*' stagedstr "✚"    # %c で表示する文字列
-    zstyle ':vcs_info:git:*' unstagedstr "✖"  # %u で表示する文字列
+  # git 用のフォーマット
+  # git のときはステージしているかどうかを表示
+  zstyle ':vcs_info:git:*' formats '(%s):%F{blue}[%f%F{red}%b%f%F{blue}]%f' '%c%u %m'
+  zstyle ':vcs_info:git:*' actionformats '(%s):[%b]' '%c%u %m' '<!%a>'
+  zstyle ':vcs_info:git:*' check-for-changes true
+  zstyle ':vcs_info:git:*' stagedstr "✚"    # %c で表示する文字列
+  zstyle ':vcs_info:git:*' unstagedstr "✖"  # %u で表示する文字列
 fi
 
 # hooks 設定
 if is-at-least 4.3.11; then
-    # git のときはフック関数を設定する
+  # git のときはフック関数を設定する
 
-    # formats '(%s)-[%b]' '%c%u %m' , actionformats '(%s)-[%b]' '%c%u %m' '<!%a>'
-    # のメッセージを設定する直前のフック関数
-    # 今回の設定の場合はformat の時は2つ, actionformats の時は3つメッセージがあるので
-    # 各関数が最大3回呼び出される。
-    zstyle ':vcs_info:git+set-message:*' hooks \
-                                            git-hook-begin \
-                                            git-untracked \
-                                            git-push-status \
-                                            git-nomerge-branch \
-                                            git-stash-count
+  # formats '(%s)-[%b]' '%c%u %m' , actionformats '(%s)-[%b]' '%c%u %m' '<!%a>'
+  # のメッセージを設定する直前のフック関数
+  # 今回の設定の場合はformat の時は2つ, actionformats の時は3つメッセージがあるので
+  # 各関数が最大3回呼び出される。
+  zstyle ':vcs_info:git+set-message:*' hooks \
+    git-hook-begin \
+    git-untracked \
+    git-push-status \
+    git-nomerge-branch \
+    git-stash-count
 
-    # フックの最初の関数
-    # git の作業コピーのあるディレクトリのみフック関数を呼び出すようにする
-    # (.git ディレクトリ内にいるときは呼び出さない)
-    # .git ディレクトリ内では git status --porcelain などがエラーになるため
-    function +vi-git-hook-begin() {
-        if [[ $(command git rev-parse --is-inside-work-tree 2> /dev/null) != 'true' ]]; then
-            # 0以外を返すとそれ以降のフック関数は呼び出されない
-            return 1
+  # フックの最初の関数
+  # git の作業コピーのあるディレクトリのみフック関数を呼び出すようにする
+  # (.git ディレクトリ内にいるときは呼び出さない)
+  # .git ディレクトリ内では git status --porcelain などがエラーになるため
+  function +vi-git-hook-begin() {
+  if [[ $(command git rev-parse --is-inside-work-tree 2> /dev/null) != 'true' ]]; then
+    # 0以外を返すとそれ以降のフック関数は呼び出されない
+    return 1
+  fi
+
+  return 0
+}
+
+# untracked ファイル表示
+#
+# untracked ファイル(バージョン管理されていないファイル)がある場合は
+# unstaged (%u) に ? を表示
+function +vi-git-untracked() {
+# zstyle formats, actionformats の2番目のメッセージのみ対象にする
+if [[ "$1" != "1" ]]; then
+  return 0
+fi
+
+if command git status --porcelain 2> /dev/null \
+  | awk '{print $1}' \
+  | command grep -F '??' > /dev/null 2>&1 ; then
+
+# unstaged (%u) に追加
+hook_com[unstaged]+='?'
         fi
+      }
 
+      # push していないコミットの件数表示
+      #
+      # リモートリポジトリに push していないコミットの件数を
+      # pN という形式で misc (%m) に表示する
+      function +vi-git-push-status() {
+      # zstyle formats, actionformats の2番目のメッセージのみ対象にする
+      if [[ "$1" != "1" ]]; then
         return 0
-    }
+      fi
 
-    # untracked ファイル表示
-    #
-    # untracked ファイル(バージョン管理されていないファイル)がある場合は
-    # unstaged (%u) に ? を表示
-    function +vi-git-untracked() {
-        # zstyle formats, actionformats の2番目のメッセージのみ対象にする
-        if [[ "$1" != "1" ]]; then
-            return 0
-        fi
+      if [[ "${hook_com[branch]}" != "master" ]]; then
+        # master ブランチでない場合は何もしない
+        return 0
+      fi
 
-        if command git status --porcelain 2> /dev/null \
-            | awk '{print $1}' \
-            | command grep -F '??' > /dev/null 2>&1 ; then
+      # push していないコミット数を取得する
+      local ahead
+      ahead=$(command git rev-list origin/master..master 2>/dev/null \
+        | wc -l \
+        | tr -d ' ')
 
-            # unstaged (%u) に追加
-            hook_com[unstaged]+='?'
-        fi
-    }
-
-    # push していないコミットの件数表示
-    #
-    # リモートリポジトリに push していないコミットの件数を
-    # pN という形式で misc (%m) に表示する
-    function +vi-git-push-status() {
-        # zstyle formats, actionformats の2番目のメッセージのみ対象にする
-        if [[ "$1" != "1" ]]; then
-            return 0
-        fi
-
-        if [[ "${hook_com[branch]}" != "master" ]]; then
-            # master ブランチでない場合は何もしない
-            return 0
-        fi
-
-        # push していないコミット数を取得する
-        local ahead
-        ahead=$(command git rev-list origin/master..master 2>/dev/null \
-            | wc -l \
-            | tr -d ' ')
-
-        if [[ "$ahead" -gt 0 ]]; then
-            # misc (%m) に追加
-            hook_com[misc]+="(p${ahead})"
-        fi
+      if [[ "$ahead" -gt 0 ]]; then
+        # misc (%m) に追加
+        hook_com[misc]+="(p${ahead})"
+      fi
     }
 
     # マージしていない件数表示
@@ -132,57 +132,57 @@ if is-at-least 4.3.11; then
     # 現在のブランチ上でまだ master にマージしていないコミットの件数を
     # (mN) という形式で misc (%m) に表示
     function +vi-git-nomerge-branch() {
-        # zstyle formats, actionformats の2番目のメッセージのみ対象にする
-        if [[ "$1" != "1" ]]; then
-            return 0
-        fi
+    # zstyle formats, actionformats の2番目のメッセージのみ対象にする
+    if [[ "$1" != "1" ]]; then
+      return 0
+    fi
 
-        if [[ "${hook_com[branch]}" == "master" ]]; then
-            # master ブランチの場合は何もしない
-            return 0
-        fi
+    if [[ "${hook_com[branch]}" == "master" ]]; then
+      # master ブランチの場合は何もしない
+      return 0
+    fi
 
-        local nomerged
-        nomerged=$(command git rev-list master..${hook_com[branch]} 2>/dev/null | wc -l | tr -d ' ')
+    local nomerged
+    nomerged=$(command git rev-list master..${hook_com[branch]} 2>/dev/null | wc -l | tr -d ' ')
 
-        if [[ "$nomerged" -gt 0 ]] ; then
-            # misc (%m) に追加
-            hook_com[misc]+="(m${nomerged})"
-        fi
-    }
+    if [[ "$nomerged" -gt 0 ]] ; then
+      # misc (%m) に追加
+      hook_com[misc]+="(m${nomerged})"
+    fi
+  }
 
 
-    # stash 件数表示
-    #
-    # stash している場合は :SN という形式で misc (%m) に表示
-    function +vi-git-stash-count() {
-        # zstyle formats, actionformats の2番目のメッセージのみ対象にする
-        if [[ "$1" != "1" ]]; then
-            return 0
-        fi
+  # stash 件数表示
+  #
+  # stash している場合は :SN という形式で misc (%m) に表示
+  function +vi-git-stash-count() {
+  # zstyle formats, actionformats の2番目のメッセージのみ対象にする
+  if [[ "$1" != "1" ]]; then
+    return 0
+  fi
 
-        local stash
-        stash=$(command git stash list 2>/dev/null | wc -l | tr -d ' ')
-        if [[ "${stash}" -gt 0 ]]; then
-            # misc (%m) に追加
-            hook_com[misc]+=":S${stash}"
-        fi
-    }
+  local stash
+  stash=$(command git stash list 2>/dev/null | wc -l | tr -d ' ')
+  if [[ "${stash}" -gt 0 ]]; then
+    # misc (%m) に追加
+    hook_com[misc]+=":S${stash}"
+  fi
+}
 fi
 
 function _update_vcs_info_msg() {
-  local -a messages
+local -a messages
 
-  LANG=en_US.UTF-8 vcs_info
-  if [[ -z ${vcs_info_msg_0_} ]]; then
-    RPROMPT=""
-  else
-    [[ -n "$vcs_info_msg_0_" ]] && messages+=("%F{green}${vcs_info_msg_0_}%f")
-    [[ -n "$vcs_info_msg_1_" ]] && messages+=("%F{yellow}${vcs_info_msg_1_}%f")
-    [[ -n "$vcs_info_msg_2_" ]] && messages+=("%F{red}${vcs_info_msg_2_}%f")
+LANG=en_US.UTF-8 vcs_info
+if [[ -z ${vcs_info_msg_0_} ]]; then
+  RPROMPT=""
+else
+  [[ -n "$vcs_info_msg_0_" ]] && messages+=("%F{green}${vcs_info_msg_0_}%f")
+  [[ -n "$vcs_info_msg_1_" ]] && messages+=("%F{yellow}${vcs_info_msg_1_}%f")
+  [[ -n "$vcs_info_msg_2_" ]] && messages+=("%F{red}${vcs_info_msg_2_}%f")
 
-    RPROMPT="${(j: :)messages}"
-  fi
+  RPROMPT="${(j: :)messages}"
+fi
 }
 add-zsh-hook precmd _update_vcs_info_msg
 
@@ -197,9 +197,9 @@ setopt prompt_subst
 
 # プロンプト指定
 ret_status=" \
-%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[blue]%}➜ %s)\
-%{${fg_bold[yellow]}%}%c%{${reset_color}%}\
-%(?.%{$fg_bold[green]%}.%{$fg_bold[blue]%}) %(?!(*･-･%) <!(*;_;%)? <)%{${reset_color}%} "
+  %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[blue]%}➜ %s)\
+  %{${fg_bold[yellow]}%}%c%{${reset_color}%}\
+  %(?.%{$fg_bold[green]%}.%{$fg_bold[blue]%}) %(?!(*･-･%) <!(*;_;%)? <)%{${reset_color}%} "
 
 PROMPT="$ret_status"
 RPROMPT="${vcs_info_msg_0_}"
@@ -249,15 +249,15 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 # manページに色付け
 export MANPAGER='less -R'
 man() {
-    env \
-        LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-        LESS_TERMCAP_md=$(printf "\e[1;31m") \
-        LESS_TERMCAP_me=$(printf "\e[0m") \
-        LESS_TERMCAP_se=$(printf "\e[0m") \
-        LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-        LESS_TERMCAP_ue=$(printf "\e[0m") \
-        LESS_TERMCAP_us=$(printf "\e[1;32m") \
-        man "$@"
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
 }
 
 ###
@@ -332,20 +332,20 @@ fi
 ### peco ###
 if which peco > /dev/null 2>&1 ; then
   function peco-select-history() {
-      local tac
-      if which tac > /dev/null; then
-          tac="tac"
-      else
-          tac="tail -r"
-      fi
-      BUFFER=$(\history -n 1 | \
-          eval $tac | \
-          peco --query "$LBUFFER")
-      CURSOR=$#BUFFER
-      zle clear-screen
-  }
-  zle -N peco-select-history
-  bindkey '^r' peco-select-history
+  local tac
+  if which tac > /dev/null; then
+    tac="tac"
+  else
+    tac="tail -r"
+  fi
+  BUFFER=$(\history -n 1 | \
+    eval $tac | \
+    peco --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
 fi
 
 ### composer global commands"
