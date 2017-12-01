4762fdc765a447b752df0dda1cafd41d02f66270
sota1235 <sota1235@gmail.com>
2017-12-01 16:57:35 +0900

    modify: add alias for getting unixtimestamp

--- zsh/alias/alias.zsh
+++ zsh/alias/alias.zsh
@@ -96,6 +96,7 @@ alias todo="find ./ -type f -print | xargs grep -n --color TODO"
 alias history="history -i"
 alias keynotesyntax="highlight -O rtf -s earendel -K 27 -k 'Courier'"
 alias keynotesyntaxwhite="highlight -O rtf -s andes -K 27 -k 'Courier'"
+alias unixtime="date +%s"
 
 alias e="exit"
 alias g="git"
