5c27244d576a9c5e4033ba37e09fe07e6839ed8c
sota1235 <sota1235@gmail.com>
2018-01-02 19:51:24 +0900

    add: script for brew install

---
+++ init/macos/Brewfile.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+brew update
+brew upgrade
+
+brew install gist
+brew install git
+brew install peco
+brew install tig
+brew install highlight
+brew install mosh
+brew install tmux
+brew install mycli
+brew install yarn
+brew install zsh
