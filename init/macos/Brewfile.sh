696df8c4af5425d4bf8ff756e823eb26abb59373
sota1235 <sota1235@gmail.com>
2018-04-17 18:40:31 +0900

    modify: add cask list and mas

--- init/macos/Brewfile.sh
+++ init/macos/Brewfile.sh
@@ -19,3 +19,32 @@ brew install ffmpeg
 brew install htop
 brew install jq
 brew install wget
+
+brew tap caskroom/cask
+
+brew cask install iterm2
+brew cask install docker
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install sequel-pro
+brew cask install insomnia
+brew cask install appcleaner
+brew cask install spotify
+brew cask install sketch
+brew cask install visual-studio-codeA
+brew cask install jetbrains-toolbox
+brew cask install karabiner-elements
+brew cask install zeplin
+brew cask install tunnelblick
+brew cask install alfred
+brew cask install firefox
+
+brew install mas
+
+mas install 409183694   # Keynote
+mas install 443987910   # 1Password
+mas install 803453959   # Slack
+mas install 539883307   # LINE
+mas install 1287239339  # ColorSlurp
+mas install 585829637   # Todoist
+mas install 441258766   # Magnet
