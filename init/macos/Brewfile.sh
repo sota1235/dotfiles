bb50db37eed185b8388d317254085b4768941d17
sota1235 <sota1235@gmail.com>
2018-04-17 16:25:27 +0900

    add: brew install list

--- init/macos/Brewfile.sh
+++ init/macos/Brewfile.sh
@@ -5,6 +5,7 @@ brew upgrade
 
 brew install gist
 brew install git
+brew install git-extras
 brew install peco
 brew install tig
 brew install highlight
@@ -14,3 +15,7 @@ brew install mycli
 brew install yarn
 brew install zsh
 brew install heroku/brew/heroku
+brew install ffmpeg
+brew install htop
+brew install jq
+brew install wget
