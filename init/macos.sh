127efbd2a3ce4169ef6dd7d2870444f9d76b8011
{git_user_name} <{git_email_address}>
2020-05-18 12:17:11 +0900

    modify: replace homebrew installer with new one

--- init/macos.sh
+++ init/macos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Installing Homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 if which brew > /dev/null 2>&1 ; then
   $HOME/.dotfiles/init/macos/Brewfile.sh
