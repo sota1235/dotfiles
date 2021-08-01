5052527f18ae3626a5499b48eee0072208c31ee2
{git_user_name} <{git_email_address}>
2021-08-01 18:35:11 +0900

    fix: load path after installing homebrew

--- init/macos.sh
+++ init/macos.sh
@@ -2,6 +2,7 @@
 
 # Installing Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+exec $SHELL -l
 
 if which brew > /dev/null 2>&1 ; then
   $HOME/.dotfiles/init/macos/Brewfile.sh
@@ -17,6 +18,5 @@ $HOME/.dotfiles/init/macos/nativefier.sh
 curl -L git.io/nodebrew | perl - setup
 
 # Install anyenv-update
-exec $SHELL -l
 mkdir -p $(anyenv root)/plugins
 git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
