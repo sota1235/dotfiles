cbd5d7d0cdc3adf44e91052fa7d830a13e72d3bf
{git_user_name} <{git_email_address}>
2021-08-01 21:44:43 +0900

    modify: remove nativifier script

--- init/macos.sh
+++ init/macos.sh
@@ -11,9 +11,6 @@ fi
 # Setting up system preference
 $HOME/.dotfiles/init/macos/os_setup.sh
 
-# Create custom apps
-$HOME/.dotfiles/init/macos/nativefier.sh
-
 # Install nodebrew
 curl -L git.io/nodebrew | perl - setup
 
